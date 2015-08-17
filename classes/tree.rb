21c6f27405c364296b8d1db943469f2c451b7a04
Khanh Luc <itsmekhanh@gmail.com>
2015-08-17 15:21:30 -0700

    added tree class

---
+++ classes/tree.rb
@@ -0,0 +1,19 @@
+class BinaryTreeNode
+    
+    attr_accessor :data
+    attr_accessor :right
+    attr_accessor :left
+
+    def initialize(data)
+        @data = data
+        @right = nil
+        @left = nil
+    end
+
+end
+
+class BinaryTree
+
+    def initialize
+    end
+end
