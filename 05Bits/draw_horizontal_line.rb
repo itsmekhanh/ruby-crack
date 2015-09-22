9667178ef368d8da8109a5cb5cc56833b71d5d2b
Khanh Luc <itsmekhanh@gmail.com>
2015-09-22 10:24:12 -0700

    added horizontal design

--- 05Bits/draw_horizontal_line.rb
+++ 05Bits/draw_horizontal_line.rb
@@ -3,4 +3,18 @@
 
 def draw_horizontal_line(screen, width, x1, x2, y)
 
+    start_index = (((y-1)*width+x1)/8).floor
+    end_index = (((y-1)*width+x2)/8).floor
+    
+    if start_index == end_index
+        screen[start_index] = (1<<(8-(x1%8))-1) | (~1^(1<<(8-(x2%8)-1)))
+    else
+        screen[start_index] = (1<<(8-(x1%8))-1)
+        start_index += 1
+
+        while start_index < end_index do
+            screen[start_index] = ~1
+        end
+        screen[end_index] = ~1^(1<< (8-(x2%8)-1))
+    end
 end
