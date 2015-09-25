65184f2ea49efb022c669e74ac39e790b769bf6d
Khanh Luc <itsmekhanh@gmail.com>
2015-09-25 16:03:59 -0700

    added stairs

--- 09RecursionDP/stairs.rb
+++ 09RecursionDP/stairs.rb
@@ -1,1 +1,19 @@
-#
+# A kid can only jump 1,2, or 3 steps at a time up the stairs. Given a height h,
+# return the number of different ways the kid can jump up the stairs.
+
+def jump(h, buffer={}) 
+    if h < 0 
+        return 0
+    elsif h == 0 
+        return 1
+    elsif buffer[h] 
+        return buffer[h] 
+    end
+
+    buffer[h] = jump(h-1, buffer)+jump(h-2, buffer)+jump(h-3, buffer)
+    return buffer[h];
+end
+
+puts "Testing jump()"
+puts "jump(6)"
+puts jump(6)
